624e0b537d2eaa6a336c97096bbe832b890a4fe1
f0e <7321764+f0e@users.noreply.github.com>
2026-09-24 18:20:42 +1000

    fix: theme scripts breaking when localstorage is blocked

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,5 +1,7 @@
 export declare global {
   var ThemeProvider: {
+    getPreference(): string;
+    setPreference(preference: string): void;
     isDarkTheme(): boolean;
     applyTheme(): void;
   };
